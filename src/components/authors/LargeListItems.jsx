ac1ab3c4790baebefcae393cab5b6b5060f8ee36
Camilo Figueroa <cigotete@gmail.com>
2023-11-23 14:01:22 -0500

    Parsing books form respective json and combine them with books in json file of each author.

--- src/components/authors/LargeListItems.jsx
+++ src/components/authors/LargeListItems.jsx
@@ -1,7 +1,25 @@
 import PropTypes from 'prop-types';
+import { books as booksData } from '../../data/books';
 
 export const LargeAuthorListItem = ({ author }) => {
   const { name, age, country, books } = author;
+  console.log('author', author);
+
+    // Filrtering correspoding books data of current author.
+    const authorBooksData = booksData.filter(book => books.includes(book.title));
+    console.log('authorBooksData', authorBooksData);
+
+    // Set of titles that are in author list but not in JSON.
+    const missingBooks = new Set(books.filter(title => !authorBooksData.some(book => book.title === title)));
+    console.log('missingBooks', missingBooks);
+  
+    // Combining books data of JSON with missing books data.
+    const combinedBooksData = [
+      ...authorBooksData,
+      ...Array.from(missingBooks).map(title => ({ title })),
+    ];
+    console.log('combinedBooksData', combinedBooksData);
+
   return (
     <>
       <h2>{ name }</h2>
@@ -9,8 +27,10 @@ export const LargeAuthorListItem = ({ author }) => {
       <p>Country: { country }</p>
       <h2>Books</h2>
       <ul>
-        { books.map((book) => (
-          <li key={book}> {book} </li>
+        {combinedBooksData.map((book) => (
+          <li key={book.title}>
+            {book.title} - Pages: {book.pages || 'N/A'} - Price: ${book.price || 'N/A'}
+          </li>
         ))}
       </ul>
     </>
