59ae1e900ec74ea622a81885fc153cd46e299ff8
Camilo Figueroa <cigotete@gmail.com>
2023-11-24 12:35:52 -0500

    Exploring a pattern for modals.

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,13 @@
-import { LargeAuthorListItem } from "./components/authors/LargeListItems";
-import { SmallAuthorListItem } from "./components/authors/SmallListItems";
+import { Modal } from "./components/Modal";
 import { LargeBookListItem } from "./components/books/LargeListItems";
-import { SmallBookListItem } from "./components/books/SmallListItems";
-import { RegularList } from "./components/lists/Regular";
-import { NumberedList } from "./components/lists/Numbered";
-import { authors } from "./data/authors";
 import { books } from "./data/books";
 
 export function App() {
   return (
     <>
-      <RegularList
-        items={ books }
-        sourceName={ "book" }
-        ItemComponent={ SmallBookListItem }
-      />
-      -----------------------------
-      <NumberedList
-        items={ books }
-        sourceName={ "book" }
-        ItemComponent={ LargeBookListItem }
-      />
-
-      <hr />
-
-      <RegularList
-        items={ authors }
-        sourceName={ "author" }
-        ItemComponent={ SmallAuthorListItem }
-      />
-      -----------------------------
-      <NumberedList
-        items={ authors }
-        sourceName={ "author" }
-        ItemComponent={ LargeAuthorListItem }
-      />
+      <Modal>
+        <LargeBookListItem book={books[0]} />
+      </Modal>
     </>
   );
 }
