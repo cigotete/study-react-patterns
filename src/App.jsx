e5f30ee20def66af32bff9d101d38a3dde62c3bb
Camilo Figueroa <cigotete@gmail.com>
2023-11-24 18:36:53 -0500

    Added CurrentUserLoader component that uses UserInfo component as child. The user data is added into child component using cloneElement.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import { CurrentUserLoader } from "./components-container/current-user-loader";
+import { UserInfo } from "./components-container/user-info";
+
 export function App() {
   return (
     <>
+      <CurrentUserLoader>
+        <UserInfo />
+      </CurrentUserLoader>
     </>
   );
 }
---
+++ src/components-container/current-user-loader.jsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+axios.defaults.baseURL = 'http://localhost:9090';
+
+export const CurrentUserLoader = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    try {
+      (async () => {
+        const response = await axios.get("/current-user");
+        setUser(response.data);
+        console.log('response.data', response.data);
+      })();
+    } catch (error) {
+      console.log('error', error);
+    }
+  }, []);
+
+  return (
+    <>
+      {React.Children.map(children, (child) => {
+        if (React.isValidElement(child)) {
+          return React.cloneElement(child, { user });
+        }
+        return child;
+      })}
+    </>
+  );
+};
+
+CurrentUserLoader.propTypes = {
+  children: PropTypes.element.isRequired,
+};
