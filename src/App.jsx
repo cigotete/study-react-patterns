ad97c1a3895012c6f450aafed3c3b930d105e5ee
Camilo Figueroa <cigotete@gmail.com>
2023-11-24 13:27:46 -0500

    App component is left empty.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
-import { Modal } from "./components/Modal";
-import { LargeBookListItem } from "./components/books/LargeListItems";
-import { books } from "./data/books";
-
 export function App() {
   return (
     <>
-      <Modal>
-        <LargeBookListItem book={books[0]} />
-      </Modal>
     </>
   );
 }
