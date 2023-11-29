3c71cd0c3a985167f55589642a20c9e672e2c33c
Camilo Figueroa <cigotete@gmail.com>
2023-11-29 01:25:12 -0500

    Adding behavior for when is finished the flow.

--- src/App-controlled-uncontrolled-components.jsx
+++ src/App-controlled-uncontrolled-components.jsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-import PropTypes from 'prop-types';
-
-import { UncontrolledForm } from "./controlled-uncontrolled/uncontrolled-form";
-import { ControlledForm } from "./controlled-uncontrolled/controlled-form";
-import { ControlledModal } from "./controlled-uncontrolled/controlled-modal";
+import PropTypes from "prop-types";
 import { UncontrolledFlow } from "./controlled-uncontrolled/uncontrolled-flow";
 
 const StepOne = ({ next }) => {
   return (
     <>
-      <h1>Step #1</h1>
-      <button onClick={next}>Next</button>
+      <h1>Step #1: Enter your name</h1>
+      <button onClick={() => next({ name: "TestName" })}>Next</button>
     </>
   );
 };
@@ -21,8 +16,8 @@ StepOne.propTypes = {
 const StepTwo = ({ next }) => {
   return (
     <>
-      <h1>Step #2</h1>
-      <button onClick={next}>Next</button>
+      <h1>Step #2: Enter your age</h1>
+      <button onClick={() => next({ age: 23 })}>Next</button>
     </>
   );
 };
@@ -33,8 +28,8 @@ StepTwo.propTypes = {
 const StepThree = ({ next }) => {
   return (
     <>
-      <h1>Step #3</h1>
-      <button onClick={next}>Next</button>
+      <h1>Step #3: Enter your country</h1>
+      <button onClick={() => next({ country: "Poland" })}>Next</button>
     </>
   );
 };
@@ -42,49 +37,19 @@ StepThree.propTypes = {
   next: PropTypes.func,
 };
 
-const DoneForm = () => {
+export function AppControlledUncontrolledComponents() {
   return (
     <>
-      <h1>Final step!</h1>
-    </>
-  );
-};
-
-export const AppControlledUncontrolledComponents = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  return (
-    <>
-
-      <h1>Controlled and Uncontrolled Components</h1>
-
-      <hr />
-      <h2>Uncontrolled Flow</h2>
-      <UncontrolledFlow>
+      <UncontrolledFlow
+        onDone={(data) => {
+          console.log(data);
+          alert("Onboarding Flow Done!");
+        }}
+      >
         <StepOne />
         <StepTwo />
         <StepThree />
-        <DoneForm />
       </UncontrolledFlow>
-
-      <hr />
-
-      <button onClick={() => setShowModal(!showModal)}>
-        {showModal ? "Hide Modal" : "Show Modal"}
-      </button>
-      <ControlledModal
-        shouldShow={showModal}
-        close={
-          () => setShowModal(false)
-        }
-      >
-        <h1>Dummy content of the modal.</h1>
-      </ControlledModal>
-
-      <hr />
-      <UncontrolledForm />
-      <hr />
-      <ControlledForm />
     </>
   );
 }
