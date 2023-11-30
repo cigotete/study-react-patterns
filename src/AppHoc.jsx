d2debab17a96a4a3383e97790189daee9ddebeff
Camilo Figueroa <cigotete@gmail.com>
2023-11-30 12:28:42 -0500

    Exploring HOC. Passing a form into a HOC to add behaviors like fetching or updating remote data.

--- src/AppHoc.jsx
+++ src/AppHoc.jsx
@@ -1,16 +1,20 @@
 // import { checkProps } from "./high-order-components/hoc-check-props";
-import { includeUserFetchingData } from "./high-order-components/include-user-fetching-data";
-import { UserInfo } from "./components-container/user-info";
+// import { includeUserFetchingData } from "./high-order-components/include-user-fetching-data";
+// import { UserInfo } from "./components-container/user-info";
+import { UserInfoForm } from "./high-order-components/user-form";
 
 // const UserInfoWrapper = checkProps(UserInfo);
-const UserInfoWithUserFetchData = includeUserFetchingData(UserInfo, "2");
+// const UserInfoWithUserFetchData = includeUserFetchingData(UserInfo, "2");
 
 export const AppHoc = () => {
   return (
     <>
     <h1>HOCs</h1>
+    <UserInfoForm />
+    {/*
     <UserInfoWithUserFetchData />
-    {/* <UserInfoWrapper propA="test1" blabla={{ a: 1, age: 23 }} /> */}
+    <UserInfoWrapper propA="test1" blabla={{ a: 1, age: 23 }} />
+    */}
     </>
   );
 }
---
+++ src/high-order-components/include-user-updating-data.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export const includeUserUpdatingTable = (Component, userId) => {
+  axios.defaults.baseURL = 'http://localhost:9090';
+  // eslint-disable-next-line react/display-name
+  return (props) => {
+    const [user, setUser] = useState(null);
+    const [updatableUser, setUpdatableUser] = useState(null);
+
+    useEffect(() => {
+      (async () => {
+        const response = await axios.get(`/users/${userId}`);
+        setUser(response.data);
+        setUpdatableUser(response.data);
+      })();
+    }, []);
+
+    const userChangeHandler = (updates) => {
+      setUpdatableUser({ ...updatableUser, ...updates });
+    };
+
+    const userPostHandler = async () => {
+      const response = await axios.post(`/users/${userId}`, {
+        user: updatableUser,
+      });
+      setUser(response.data);
+      setUpdatableUser(response.data);
+    };
+
+    const resetUserHandler = () => {
+      setUpdatableUser(user);
+    };
+
+    return (
+      <Component
+        {...props}
+        updatableUser={updatableUser}
+        changeHandler={userChangeHandler}
+        userPostHandler={userPostHandler}
+        resetUserHandler={resetUserHandler}
+      />
+    );
+  };
+};
