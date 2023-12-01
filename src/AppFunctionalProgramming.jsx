afd3daaf9a868dc6dbfd89c853f6d0d2422db18f
Camilo Figueroa <cigotete@gmail.com>
2023-12-01 00:01:01 -0500

    Functional programming. Composition with components.

--- src/AppFunctionalProgramming.jsx
+++ src/AppFunctionalProgramming.jsx
@@ -1,4 +1,5 @@
 import { Recursive } from "./functional-programming/recursive";
+import { SmallButton, SmallRedButton } from "./functional-programming/composition";
 
 const myNestedObject = {
   key1: "value1",
@@ -15,6 +16,8 @@ const myNestedObject = {
 export function AppFunctionalProgramming() {
   return (
     <>
+      <SmallButton text={"I am small!"}/>
+      <SmallRedButton text={"I am small and Red"}/>
       <Recursive data={myNestedObject} />
     </>
   );
